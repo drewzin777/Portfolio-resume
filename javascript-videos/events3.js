ef38654a517c9551719ab11dd168027ce1f0b377
Andrew Amento <surandrew777@gmail.com>
2024-07-26 17:19:04 -0700

    JS events
    added file for events practice

---
+++ javascript-videos/events3.js
@@ -0,0 +1,12 @@
+const box1 = document.getElementById("box1"); 
+const box2 = document.getElementById("box2"); 
+
+box1.addEventListener("click", () => {
+    box1.style.backgroundColor = box1.style.backgroundColor === "red" ? "green" : "red";
+    
+}); 
+
+box2.addEventListener("mouseover", () => {
+    box2.style.backgroundColor = box2.style.backgroundColor === "blue" ? "orange" : "blue";
+    
+});
