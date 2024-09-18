d5a5d8b7a39bee3486373e4364f0aa5481c66a18
Andrew Amento <surandrew777@gmail.com>
2024-09-18 15:11:25 -0700

    php practice
    practice php

---
+++ javascript-videos/index.php/integers.php
@@ -0,0 +1,23 @@
+<?php
+            $hoursInYear = 8760;
+            $minutesInYear = 525600;
+            $secondsInYear = 31536000; 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            PHP Data Types -- Integers
+        </title>
+    </head>
+    <body>
+        <?php
+            echo "<p>There are $hoursInYear hours in a year </p>";
+            echo "<p>There are $minutesInYear minutes in a year </p>";
+            echo "<p>There are $secondsInYear seconds in a year </p>"; 
+        ?>
+        
+    </body>
+</html>
