e1e2ccb83bd9f2376a16a3d5fad88d22a59914c2
Andrew Amento <surandrew777@gmail.com>
2024-09-18 11:47:06 -0700

    learn php
    cont. to practice with PHP files

---
+++ javascript-videos/index.php/labExercise2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab Exercise 2</title>
+</head>
+<body>
+    <?php 
+        
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+    
+        //Learning more from the videos about how to write PHP
+        echo "<h2>These are some of the main style rules when coding PHP.</h2>"; 
+        echo "<p>As we move along in the course, you will learn more syntax about more specific parts of PHP, like functions and arrays.</p>";
+        echo "In the next episode, we will learn types and variables.";
+    ?>
+</body>
+</html>
