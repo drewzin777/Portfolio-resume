05dc95b8bdbd234ceb0ea20779b28c93966c1b1a
Andrew Amento <surandrew777@gmail.com>
2024-09-18 16:36:43 -0700

    php
    php files learning

---
+++ javascript-videos/hello_world_with_errors.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>Hello World from PHP</title>
+    </head>
+<body>
+    <?php
+   echo "<h1>Hello World!</h1>";
+   echo "<h2>My name is Andrew</h2>";
+   echo "<p>I am a bass player.</p>";
+   echo "<p>I have four kids. Two boys and two girls.</p>";
+    ?>
+</body>
+</html>
