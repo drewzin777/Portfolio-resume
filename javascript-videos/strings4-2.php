594405f66c7b0649f551e58fa1aab539c58fa878
Andrew Amento <surandrew777@gmail.com>
2024-09-27 12:51:35 -0700

    php
    learning PHP

---
+++ javascript-videos/strings4-2.php
@@ -0,0 +1,18 @@
+<?php
+    $h = "Hydrogen"; 
+    $o = "Oxygen"; 
+    $w = "Water"; 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ß, initial-scale=1.0">
+    <title>Strings Ch 4 Lab4.2</title>
+</head>
+<body>
+    <?php 
+        echo "Two molecules of $h and one molecule of $o make one molecule of $w.";
+    ?>
+</body>
+</html>
