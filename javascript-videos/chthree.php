8b7feae7aacf96a857c343bbe4c6f39e2af92252
Andrew Amento <surandrew777@gmail.com>
2024-09-27 08:58:44 -0700

    php
    working on leaning PHP

---
+++ javascript-videos/chthree.php
@@ -0,0 +1,28 @@
+<?php
+$age = 18; 
+$olderAge = 25; 
+$yearsPast = 7; 
+
+// Test each variable separately
+echo "Age: $age<br>"; 
+echo "Older Age: $olderAge<br>"; 
+echo "Years Past: $yearsPast<br>";
+
+// Now test the full sentence
+echo "Mark Twain once said \"When I was $age my father knew nothing about the world. Now that I'm $olderAge, I'm surprised to see how much the old man has learned in the past $yearsPast years.\"";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Data Types Exercise</title>
+</head>
+<body>
+    <?php 
+        echo "Mark Twain once said \"When I was $age my father knew nothing about the world. Now that i'm $olderAge, I'm surprised to see how much the old man has learned in the past $yearsPast years.\"";
+    ?>
+</body>
+</html>
